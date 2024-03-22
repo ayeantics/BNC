1ff148ae3ac4122309ea7cec02d60bf5a4c2eb00
ayeantics <nizharadze.tsotne6@gmail.com>
2024-03-22 20:29:22 +0400

    burger menu

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,17 @@ function toggleNav() {
   const navLinksWrapper = document.getElementById("navLinksWrapper");
   const menuIcon = document.getElementById("menuIcon");
 
-  navLinksWrapper.classList.toggle("show");
-
   if (navLinksWrapper.classList.contains("show")) {
-    menuIcon.classList.remove("bi-list");
-    menuIcon.classList.add("bi-x-lg");
-  } else {
+    navLinksWrapper.classList.add("hide"); // Add the hide class first
+    setTimeout(() => {
+      navLinksWrapper.classList.remove("show"); // Then remove the show class after a short delay
+    }, 300); // Adjust the delay as needed to match your animation duration
     menuIcon.classList.remove("bi-x-lg");
     menuIcon.classList.add("bi-list");
+  } else {
+    menuIcon.classList.remove("bi-list");
+    menuIcon.classList.add("bi-x-lg");
+    navLinksWrapper.classList.remove("hide"); // Remove the hide class when opening
+    navLinksWrapper.classList.add("show");
   }
 }
