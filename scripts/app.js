d17d08d1c94ff376e9bb829ba2fff367de62b524
ayeantics <nizharadze.tsotne6@gmail.com>
2024-04-22 21:08:47 +0400

    bevri update

--- scripts/app.js
+++ scripts/app.js
@@ -19,3 +19,33 @@ function toggleNav() {
     navLinksWrapper.classList.add("show");
   }
 }
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get all images and insert them inside the modal - use their "alt" text as a caption
+let images = document.querySelectorAll(".roof-image img");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+images.forEach((img) => {
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+});
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Also close the modal if the user clicks outside of the image
+modal.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
